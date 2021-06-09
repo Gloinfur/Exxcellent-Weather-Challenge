ae0939010bbe5b72fd16b7518d3a352e555dcd57
Jan P <gloinfur@gmail.com>
2021-06-09 22:36:55 +0200

    Initialized project structure.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -13,12 +13,27 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        weatherTask();
+        footballTask();
+    }
 
-        // Your preparation code …
-
+    /**
+     * Starts challenge task 1: Determine the day with the minimal spread between the maxTemperature and minTemperature from the given data
+     */
+    public static void weatherTask() {
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+    }
 
+    /**
+     * Starts challenge task 2: Determine the team with the minimal spread (absolute difference) between scored goals ('Goals') and received goals ('Goals allowed') from the given data.
+     * Interpreting the minimal spread ('absolute difference') in this task as the smallest difference between scored goals und received goals:
+     * E.g. (Goals | Goals Allowed):
+     * 10 | 5 => 10 - 5 = 5
+     * 40 | 50 => 40 - 50 = -10
+     * 5 is the smallest difference between scored goals and received goals.
+     */
+    public static void footballTask() {
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
